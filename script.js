88c2652a8a52ad8b33867c39796db607f290f501
Fmquito45 <fmquitox45@gmail.com>
2023-08-23 23:21:04 -0400

    adds confirms to prompts

--- script.js
+++ script.js
@@ -15,9 +15,21 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
- var charLength = prompt ('How many characters would you like your password to contain?')
- if (isNaN(charLength)){
-  alert ("Please insert valid Numeric Number!!!")
+ var charLength = prompt ("How many characters would you like your password to contain?");
+ if (isNaN(charLength) || Number(charLength)<8 || Number(charLength)>128){
+  alert ("Please insert valid Numeric characters!!! \nChoose a length of at least 8 characters and no more than 128 character.")
  }
 
+var specicalChars = '!@#$%^&*()=+-[]{};:,./?'
+var nums = '0123456789' 
+var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz'
+var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+var isSpecialChar = confirm ("Click OK to confrim $pecial characters");
+var isNumericChar = confirm ("Click OK to confrim #numeric characters");
+var isLowerChar = confirm ("Click OK to confrim lowercase characters");
+var isUpperChar = confirm ("Click OK to confrim UPPERCASE characters");
+
+if ()
 } 
