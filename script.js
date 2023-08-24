4a2adcd46111d2c5452a69cea0bdaba4601e43b7
Fmquito45 <fmquitox45@gmail.com>
2023-08-24 15:12:23 -0400

    adds comments to JS file

--- script.js
+++ script.js
@@ -13,24 +13,25 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+//sets prompt to accept valid number length to continue with function
 function generatePassword() {
  var charLength = prompt ("How many characters would you like your password to contain?");
  if (isNaN(charLength) || Number(charLength)<8 || Number(charLength)>128){
   return alert ("Please insert valid Numeric characters!!! \nChoose a length of at least 8 characters and no more than 128 character.")
  } 
-
+// multiple variables set with their assigned characters
 var specialChars = '!@#$%^&*()=+-[]{};:,./?'
 var nums = '0123456789' 
 var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz'
 var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-
+//variables to confirm selected characters
 var isSpecialChar = confirm ("Click OK to confrim $pecial characters");
 var isNumericChar = confirm ("Click OK to confrim #numeric characters");
 var isLowerChar = confirm ("Click OK to confrim lowercase characters");
 var isUpperChar = confirm ("Click OK to confrim UPPERCASE characters");
 
+//sets function to add selected characters into var characters
 if (isSpecialChar || isNumericChar || isLowerChar || isUpperChar){
   var characters = ''
   if (isSpecialChar){
@@ -52,7 +53,9 @@ if (isSpecialChar || isNumericChar || isLowerChar || isUpperChar){
       counter += 1;
     }
     return result;
-} else {
+} 
+//if no character group is selected, alert indicates to select at least one.
+  else {
   alert ("Select at least one character type!")
   return ''
 }
